254c83dfab17706f83a1bf925fc51f1cd2fd808e
DanFogelberg <dan.f.fogelberg@gmail.com>
2023-08-22 15:11:05 +0200

    Started landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,58 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+//import { useState } from 'react'
+//import reactLogo from './assets/react.svg'
+//import viteLogo from '/vite.svg' Kept to remember how this is done
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  //const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className = "banner">
+        <p className = "bannerName">"Namn!"</p>
+        <div className = "nav">
+          <a className = "navItem navSelected">Hej</a>
+          <a className = "navItem">About us</a>
+        </div>
+
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+
+      <h1>Koncept</h1>
+
+      <div className = "overview">
+        <div className="concept">
+          <p className="circle"></p>
+          <p className = "conceptTitle">1. Onlinetest</p>
+        </div>
+        <div className="concept">
+          <p className="circle"></p>
+          <p className = "conceptTitle">2. Escape room</p>
+        </div>        <div className="concept">
+          <p className="circle"></p>
+          <p className = "conceptTitle">3. Intervju</p>
+        </div>
+
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div className = "infoBlock">
+      <p>Vi har valt att utforma urvalsprocessen som ett såll för att sortera ut sökande och minska antalet intervjuer som behöver göras. Detta i syfte att effektivisera och minska arbetet som behöver göras ifall utbildningen har mycket fler sökande än platser.</p>
+      </div>
+
+      <div className = "infoBlock">
+      <h2>Onlinemomentet</h2>
+      <p>Ett onlinetest med frågor på en grund- till medelnivå som har i syfte att dels ge de sökande ett första smakprov på vad programmet kommer att innehålla, men också för att sålla bort slentriansökande.</p>
+      </div>
+      
+      <div className = "infoBlock">
+      <h2>Escape room</h2>
+      <p>Ett “escape room”-moment på plats i Yrgos lokaler som skall testa problemlösnings- och samarbetsförmåga hos sökande. Det sista momentet i escape room:et ger den sökande ett formulär att fylla i inför det tredje momentet. Detta moment involverar de X antal sökande som fått bäst resultat på moment 1.</p>
+      </div>
+
+      <div className = "infoBlock">
+      <h2>Intervju</h2>
+      <p>En intervju med ansvarig lärare och en person till för att gå igenom resultaten av de tidigare testerna och bilda ett intryck av den sökande för att avgöra slutgiltig lämplighet för utbildningen.</p>
+      </div>
+
+
     </>
   )
 }
