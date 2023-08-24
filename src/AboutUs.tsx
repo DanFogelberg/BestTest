9a0950fa44bd7d68f80a5140c9b0e17793f7507a
DanFogelberg <dan.f.fogelberg@gmail.com>
2023-08-24 17:45:07 +0200

    About us 98%

--- src/AboutUs.tsx
+++ src/AboutUs.tsx
@@ -1,34 +1,34 @@
 export default function AboutUs() {
     return <>
         <h1>Om oss</h1>
-        <div className = "infoBlock">
+        <div className = "aboutUsInfoBlock">
         <p>Ett onlinetest med frågor på en grund- till medelnivå som har i syfte att dels ge de sökande ett första smakprov på vad programmet kommer att innehålla, och också för att sålla bort slentriansökande som inte är så intresserade egentligen.</p>
         </div>
 
         <div className = "usInfo">
             <div className="person">
-                <img src='./images/interview.png' className="circle"></img>
+                <img src='./images/jovin.jpeg' className="circle"></img>
                 <p>Jovin Fast &#10; Game programmer</p>
             </div>
             
             <div className="person">
-                <img src='./images/interview.png' className="circle"></img>
+                <img src='./images/carl.jpeg' className="circle"></img>
                 <p>Carl Zeidler &#10; Game programmer</p>
             </div>       
 
             <div className="person">
-                <img src='./images/interview.png' className="circle"></img>
+                <img src='./images/dan.jpeg' className="circle"></img>
                 <p>Dan Fogelberg &#10; Webbutveckling</p>
                 
             </div>
 
             <div className="person">
-            <img src='./images/interview.png' className="circle"></img>
+            <img src='./images/michaela.jpeg' className="circle"></img>
                 <p>Michaela Rudbeck &#10; Ux designer</p>
             </div>
 
             <div className="person">
-            <img src='./images/interview.png' className="circle"></img>
+            <img src='./images/weronika.jpeg' className="circle"></img>
                 <p>Weronika Dudzik &#10; Ux designer</p>
             </div>
 
